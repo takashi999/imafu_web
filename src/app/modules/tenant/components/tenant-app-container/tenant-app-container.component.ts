bf673719c44696dd5b0180a8e599ebbb1046fef0
tada-tsu <tada.purple.haze@gmail.com>
2021-11-23 17:48:44 +0900

    店舗プレビューリンク

--- src/app/modules/tenant/components/tenant-app-container/tenant-app-container.component.ts
+++ src/app/modules/tenant/components/tenant-app-container/tenant-app-container.component.ts
@@ -1,5 +1,9 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TenantAuthService } from 'src/app/services/tenant/api/tenant-auth.service';
+import { TenantBasicService } from 'src/app/services/tenant/api/tenant-basic.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-tenant-app-container',
@@ -9,12 +13,20 @@ import { TenantAuthService } from 'src/app/services/tenant/api/tenant-auth.servi
 })
 export class TenantAppContainerComponent implements OnInit {
 
+  tenantLink$ = this.tenantBasicService.tenantId$.pipe(
+    filter((v): v is Exclude<typeof v, null> => v !== null),
+    map(v => this.domSanitizer.bypassSecurityTrustResourceUrl(`${ environment.serverUrl }/shop/${ v }`)),
+  );
+
   constructor(
     public tenantAuthService: TenantAuthService,
+    public tenantBasicService: TenantBasicService,
+    private domSanitizer: DomSanitizer,
   ) {
   }
 
   ngOnInit(): void {
+    this.tenantBasicService.get().subscribe();
   }
 
 }
--- src/app/services/tenant/api/tenant-basic.service.ts
+++ src/app/services/tenant/api/tenant-basic.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ModifyTenantByTenantRequest } from 'src/app/services/tenant/api/requests';
 import { Tenant } from 'src/app/services/tenant/api/responses';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TenantBasicService {
+
+  tenantId$ = new BehaviorSubject<null | number>(null);
 
   constructor(
     private httpClient: HttpClient,
@@ -14,10 +18,15 @@ export class TenantBasicService {
   }
 
   get() {
-    return this.httpClient.get<Tenant>('@te/basic');
+    return this.httpClient.get<Tenant>('@te/basic')
+      .pipe(
+        tap(x => this.tenantId$.next(x.id)),
+      );
   }
 
   modify(body: ModifyTenantByTenantRequest) {
-    return this.httpClient.patch<Tenant>('@te/basic', body);
+    return this.httpClient.patch<Tenant>('@te/basic', body).pipe(
+      tap(x => this.tenantId$.next(x.id)),
+    );
   }
 }
