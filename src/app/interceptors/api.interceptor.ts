fa3d4c8e5f9cab003b3a04a206417d25431fb6a7
tada-tsu <tada.purple.haze@gmail.com>
2022-01-05 16:07:46 +0900

    スピナー非表示をTeardownLogic内に配置、cancelledやabortに対応

--- src/app/interceptors/api.interceptor.ts
+++ src/app/interceptors/api.interceptor.ts
@@ -75,75 +75,76 @@ export class APIInterceptor implements HttpInterceptor {
       }
       this.incrementCount++;
 
-      return next.handle(
-        request.clone({
-          url: request.url.replace(new RegExp('^' + endpoint.replacement), endpoint.endpoint),
-          setHeaders: {
-            Authorization: `Bearer ${ this.tokenService.tokens[endpoint.tokenKey] }`,
-          },
-        }),
-      ).pipe(
-        retryWhen(errors => {
-          return errors.pipe(
-            mergeMap(error => {
-              if (error instanceof HttpErrorResponse) {
-                if (error.status === 429) {
-                  const retrySeconds = parseInt(error.headers.get('Retry-After') ?? ``, 10);
-                  const ref = this.matSnackBar.open(`短期間でのアクセスが多すぎました、${ retrySeconds }秒後にリトライします`, '閉じる', {
-                    duration: retrySeconds * 1000,
-                  });
-                  return timer(retrySeconds * 1000);
+      return new Observable(subscriber => {
+        next.handle(
+          request.clone({
+            url: request.url.replace(new RegExp('^' + endpoint.replacement), endpoint.endpoint),
+            setHeaders: {
+              Authorization: `Bearer ${ this.tokenService.tokens[endpoint.tokenKey] }`,
+            },
+          }),
+        ).pipe(
+          retryWhen(errors => {
+            return errors.pipe(
+              mergeMap(error => {
+                if (error instanceof HttpErrorResponse) {
+                  if (error.status === 429) {
+                    const retrySeconds = parseInt(error.headers.get('Retry-After') ?? ``, 10);
+                    const ref = this.matSnackBar.open(`短期間でのアクセスが多すぎました、${ retrySeconds }秒後にリトライします`, '閉じる', {
+                      duration: retrySeconds * 1000,
+                    });
+                    return timer(retrySeconds * 1000);
+                  }
+                }
+
+                return throwError(error);
+              }),
+            );
+          }),
+          tap({
+            error: err => {
+              if (err instanceof HttpErrorResponse) {
+                if (err.status === 401) {
+                  this.tokenService.resetToken(endpoint.tokenKey);
+                  location.reload();
+                }
+                if (err.status === 422 && err.error?.message) {
+                  const message = this.getValidationErrors(err);
+                  const ref = this.matDialog.open(APIErrorComponent);
+                  ref.componentInstance.message = `リクエストエラー\n\n${ message }`;
                 }
               }
+            },
+            complete: () => {
+              if (!request.context.get(SILENT_SNACK)) {
+                if (request.method === 'POST') {
+                  this.matSnackBar.open('作成が完了しました', '閉じる', {
+                    duration: 3000,
+                  });
+                }
+                if (request.method === 'PATCH') {
+                  this.matSnackBar.open('更新が完了しました', '閉じる', {
+                    duration: 3000,
+                  });
+                }
+                if (request.method === 'DELETE') {
+                  this.matSnackBar.open('削除が完了しました', '閉じる', {
+                    duration: 3000,
+                  });
+                }
+              }
+            },
+          }),
+        )
+          .subscribe(subscriber);
 
-              return throwError(error);
-            }),
-          );
-        }),
-        tap({
-          error: err => {
-            this.incrementCount--;
-            if (this.incrementCount === 0) {
-              this.overlayRef.detach();
-            }
-
-            if (err instanceof HttpErrorResponse) {
-              if (err.status === 401) {
-                this.tokenService.resetToken(endpoint.tokenKey);
-                location.reload();
-              }
-              if (err.status === 422 && err.error?.message) {
-                const message = this.getValidationErrors(err);
-                const ref = this.matDialog.open(APIErrorComponent);
-                ref.componentInstance.message = `リクエストエラー\n\n${ message }`;
-              }
-            }
-          },
-          complete: () => {
-            this.incrementCount--;
-            if (this.incrementCount === 0) {
-              this.overlayRef.detach();
-            }
-            if (!request.context.get(SILENT_SNACK)) {
-              if (request.method === 'POST') {
-                this.matSnackBar.open('作成が完了しました', '閉じる', {
-                  duration: 3000,
-                });
-              }
-              if (request.method === 'PATCH') {
-                this.matSnackBar.open('更新が完了しました', '閉じる', {
-                  duration: 3000,
-                });
-              }
-              if (request.method === 'DELETE') {
-                this.matSnackBar.open('削除が完了しました', '閉じる', {
-                  duration: 3000,
-                });
-              }
-            }
-          },
-        }),
-      );
+        return () => {
+          this.incrementCount--;
+          if (this.incrementCount === 0) {
+            this.overlayRef.detach();
+          }
+        };
+      });
     }
 
     return next.handle(request);
